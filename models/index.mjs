0d3870f64e707bccbd60eb88e927c602c7c69c01
kennethongcs <kenneth.ongcs@gmail.com>
2022-06-17 22:32:39 +0800

    add imports / relations

---
+++ models/index.mjs
@@ -0,0 +1,28 @@
+import sequelizePackage, { Sequelize } from 'sequelize';
+import allConfig from 'config';
+
+import initGameModel from './game-model.mjs';
+import initUserModel from './user-model.mjs';
+
+const { Sequelize } = sequelizePackage;
+const env = process.env.NODE_ENV || 'development';
+const config = allConfig[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.user,
+  config.password,
+  config
+);
+
+db.User = initUserModel(sequelize, Sequelize.DataTypes);
+db.Game = initGameModel(sequelize, Sequelize.DataTypes);
+
+db.User.hasMany(db.Game, { through: 'user_games' });
+db.Game.hasMany(db.User, { through: 'user_games' });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
