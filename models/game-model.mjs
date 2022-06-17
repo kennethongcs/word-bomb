d09bbde1d701f5dd30aa2490a03c057e1851e7ed
kennethongcs <kenneth.ongcs@gmail.com>
2022-06-17 22:32:23 +0800

    add underscored

--- models/game-model.mjs
+++ models/game-model.mjs
@@ -1,22 +1,26 @@
 export default function initGameModel(sequelize, DataTypes) {
-  return sequelize.define('game', {
-    id: {
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
+  return sequelize.define(
+    'game',
+    {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      game_state: {
+        allowNull: false,
+        type: DataTypes.JSON,
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
     },
-    game_state: {
-      allowNull: false,
-      type: DataTypes.JSON,
-    },
-    created_at: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-    updated_at: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-  });
+    { underscored: true }
+  );
 }
