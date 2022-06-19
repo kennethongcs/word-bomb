2e9747aea13cd6ed49dcd09e94c8625c3ef90362
kennethongcs <kenneth.ongcs@gmail.com>
2022-06-19 23:56:09 +0800

    trying to find game window size

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const docWidth = document.querySelector('body').clientWidth;
+const docHeight = document.querySelector('body').clientHeight;
+
+console.log('width', docWidth);
+console.log('height', docHeight);
