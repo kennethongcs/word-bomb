fe973d4cd749fa468f71cc32587cbc5efa7a9566
kennethongcs <kenneth.ongcs@gmail.com>
2022-06-23 15:04:59 +0800

    add words table

---
+++ migrations/20220622130125-words-table.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('words', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      word: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('words', null, {});
+  },
+};
