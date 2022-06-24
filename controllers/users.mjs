a1af623a4d5e3eceed90b1b129a7e512005e5c4e
kennethongcs <kenneth.ongcs@gmail.com>
2022-06-24 22:43:05 +0800

    add salt func and login verification

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -1,4 +1,20 @@
 import jsSHA from 'jssha';
+import express from 'express';
+
+const app = express();
+
+const getHashSalted = (input) => {
+  // create new SHA object
+  const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+  const SALT = 'dfaenl431';
+  // create an unhashed cookie string based on user ID and salt
+  const unhashedString = `${input}-${SALT}`;
+
+  // generate a hashed cookie string using SHA object
+  shaObj.update(unhashedString);
+
+  return shaObj.getHash('HEX');
+};
 
 export default function initUsersController(db) {
   const root = (req, res) => {
@@ -6,22 +22,30 @@ export default function initUsersController(db) {
   };
 
   const login = async (req, res) => {
-    console.log(req.body);
     try {
-      const user = await db.User.findOne({
+      const userDetails = await db.User.findOne({
         where: {
           email: req.body.email,
         },
       });
-      console.log('user', user);
+      // console.log('user', user);
+
+      const user = await db.User.findOne({
+        where: {
+          email: req.body.email,
+        },
+        attributes: { exclude: ['password'] },
+      });
 
       const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
       shaObj.update(req.body.password);
       const hashedPassword = shaObj.getHash('HEX');
 
-      if (hashedPassword === user.password) {
+      if (hashedPassword === userDetails.password) {
         res.cookie('loggedIn', true);
-        res.cookie('userId', user.id);
+        res.cookie('userId', userDetails.id);
+        const saltedUserId = getHashSalted(userDetails.Id);
+        res.cookie('sessionId', saltedUserId);
         res.send({ user });
       } else {
         console.log('not logged in ');
@@ -30,6 +54,24 @@ export default function initUsersController(db) {
       console.log(error);
     }
   };
+
+  // const loginCheck = async (req, res, next) => {
+  //   req.isUserLoggedIn = false;
+  //   if (req.cookies.userId && req.cookies.sessionId) {
+  //     const hash = getHashSalted(req.cookies.userId);
+  //     if (req.cookies.sessionId === hash) {
+  //       req.isUserLoggedIn = true;
+  //       req.user = req.cookies.userId;
+  //       next();
+  //     }
+  //   }
+  //   next();
+  // };
+
+  // const userState = async (req, res, next) => {
+  //   app.locals.user = req.user;
+  //   next();
+  // };
 
   return { root, login };
 }
